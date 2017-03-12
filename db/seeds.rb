e7ed30b18cd95659e7da326bf8fedf752e505e4f
Joey Li <joeyzaozaoli@gmail.com>
2017-03-12 08:14:17 -0700

    revise assignment

--- db/seeds.rb
+++ db/seeds.rb
@@ -18,12 +18,11 @@ end
 puts "number of posts #{Post.count}"
 x = Post.find_or_create_by(title: "Hello World", body: "I am halfway through backend.")
 puts "number of posts #{Post.count}"
-puts x
 
 puts "number of comments #{Comment.count}"
 y = Comment.find_or_create_by(post:x, body: "Watched the therapy of rejection.")
 puts "number of comments #{Comment.count}"
-puts y.post
+puts "#{y.post.title} - This indicates successful linkage post and comment."
 
 puts "Seed finished"
 puts "#{Post.count} posts created"
