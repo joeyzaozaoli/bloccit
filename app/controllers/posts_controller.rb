46b5fae9b672bd5e31eff96f71927d607e2b1bba
Joey Li <joeyzaozaoli@gmail.com>
2017-03-13 09:13:26 -0700

    mark some posts as SPAM

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,13 @@
 class PostsController < ApplicationController
   def index
     @posts = Post.all
+    @posts.map.with_index do |post, index|
+      if index % 5 == 0
+        post.title = "SPAM"
+      else
+        post.title = post.title
+      end
+    end
   end
 
   def show
@@ -12,3 +19,26 @@ class PostsController < ApplicationController
   def edit
   end
 end
+
+=begin
+The following appears that it should have worked, but it doesn't. Why?
+def index
+  @posts = Post.all.map.with_index do |post, index| # Vs. solution, I chained the map method.
+    if index % 5 == 0
+      post.title = "SPAM"
+    else
+      post.title = post.title
+    end
+  end
+end
+
+The following doesn't appear to have worked, but it does. Why?
+def index
+  @posts = Post.all
+  @posts.each.with_index do |post, index| # Vs. map method, each method should just return the original array.
+    if index % 5 == 0
+      post.title = "SPAM"
+    end
+  end
+end
+=end
