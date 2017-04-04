99491f2f0042c490495bfc73dcc0525eda5c6d29
Joey Li <joeyzaozaoli@gmail.com>
2017-04-04 08:06:59 -0700

    send email upon new post

--- app/models/post.rb
+++ app/models/post.rb
@@ -12,6 +12,8 @@ class Post < ActiveRecord::Base
   validates :topic, presence: true
   validates :user, presence: true
 
+  after_create :favor_and_send_email
+
   def up_votes
     votes.where(value: 1).count
   end
@@ -28,5 +30,12 @@ class Post < ActiveRecord::Base
     age_in_days = (created_at - Time.new(1970,1,1)) / 1.day.seconds
     new_rank = points + age_in_days
     update_attribute(:rank, new_rank)
+  end
+
+  private
+
+  def favor_and_send_email
+    self.favorites.create(user: self.user)
+    FavoriteMailer.new_post(self.user, self).deliver_now
   end
 end
